274016f5da8c6727e7646f77be8d9b1df579a53a
NikolaMitic16228 <mitic.nikola@elfak.rs>
2020-01-15 05:51:31 +0100

    deleted some stuff

--- src/services/SumeService.js
+++ src/services/SumeService.js
@@ -3,26 +3,7 @@ export default class SumeService {
     constructor() {
         this.baseURL = 'http://localhost:4000';
     }
-    getAllTrees() {
-        return axios.get(`${this.baseURL}/AllTress`);
-    }
-    getAllRegions() {
-        return axios.get(`${this.baseURL}/AllRegions`);
-    }
-
-    deleteTree(treeID) {
-        return axios.post(`${this.baseURL}/deleteTree`, { params:{ drvo: treeID }});
-    }
-
-    setNewStatistics(regId, treeId, plantNum, cutsNum, plantSpace, cutsSpace) {
-        return axios.post(`${this.baseURL}/newStatistics`,
-        { params:{ region: regId, drvo: treeId, brPosecena: cutsNum, brZasadjena: plantNum, povrsPosecena: cutsSpace, povrsZasadjena: plantSpace }});
-    }
-    setNewTreeAndStatistics(typeId, name, regId, plantNum, cutsNum, plantSpace, cutsSpace) {
-        return axios.post(`${this.baseURL}/newTreeAndStatistics`,
-        { params:{ tip: typeId, naziv: name, region: regId, brPosecena: cutsNum, brZasadjena: plantNum, povrsPosecena: cutsSpace, povrsZasadjena: plantSpace }});
-    }
-
+    
     getYearPlantings() {
         return axios.get(`${this.baseURL}/AllPlantings`);
     }
